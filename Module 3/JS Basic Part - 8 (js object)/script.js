0ec247f3d48429994c4445448a1163c749cde26c
DustyCube <113673336+Dusty-Cube@users.noreply.github.com>
2023-06-26 22:56:09 +0600

    js object

---
+++ Module 3/JS Basic Part - 8 (js object)/script.js
@@ -0,0 +1,14 @@
+var info = {
+    firstName: 'Nasim',
+    lastName: 'Rahaman',
+    age: 22,
+    warning: alert('TEST')
+}
+
+console.log(info);
+
+console.log(info.lastName);
+
+console.log(info['age']);
+
+info.warning;
