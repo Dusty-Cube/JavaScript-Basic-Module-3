54811f0f63bfdfe10972b000deed69553f1424aa
DustyCube <113673336+Dusty-Cube@users.noreply.github.com>
2023-06-26 16:05:42 +0600

    switch case

---
+++ Module 3/JS Basic Part - 5 (switch-case)/script.js
@@ -0,0 +1,29 @@
+var num = 55 % 25;
+
+switch(num){
+    case 0:
+        console.log(0); break;
+    case 5:
+        console.log(5); break;
+    case 10:
+        console.log(10); break;
+    case 15:
+        console.log(15); break;
+    case 20:
+        console.log(20); break;
+    case 25:
+        console.log(25); break;
+}
+
+var day = 'Friday';
+
+switch(day){
+    case 'Friday':
+        console.log("This is Friday"); break;
+    case 'Saturday':
+        console.log("This is Saturday"); break;
+    case 'Sunday':
+        console.log("This is Sunday"); break;
+    case 'Monday':
+        console.log("This is Monday"); break;
+}
