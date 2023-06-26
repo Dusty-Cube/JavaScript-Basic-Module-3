628150ba16982d8b48571e19164a16c1c723e237
DustyCube <113673336+Dusty-Cube@users.noreply.github.com>
2023-06-26 19:18:17 +0600

    for do while loop

---
+++ Module 3/JS Basic Part - 6 (for-do-while-loop)/script.js
@@ -0,0 +1,19 @@
+var i = 0;
+
+for(i; i <= 20; i++)
+{
+    console.log(i + ' text');
+}
+
+var num = 55;
+
+do{
+    console.log(num);
+    num--;
+}while(num >= 45);
+
+while(num <= 55)
+{
+    console.log(num);
+    num++;
+}
